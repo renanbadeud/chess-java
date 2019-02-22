4f78391be81f8667f162ec7164a3f9f79ba483dd
Renan <renan.bad@live.com>
2019-02-22 11:05:51 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -49,6 +49,10 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : "+ chessMatch.getTurn());
 		System.out.println("Waiting player: "+ chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
+		
 	}
 	
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
